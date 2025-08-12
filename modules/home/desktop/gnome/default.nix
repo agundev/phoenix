796f29f7398f0085a3a2a8d422396abbaf90cb42
agundev <advaith@gundu.me>
2025-08-12 13:41:35 -0400

    feat: add aatws

--- modules/home/desktop/gnome/default.nix
+++ modules/home/desktop/gnome/default.nix
@@ -56,6 +56,7 @@ in
           brightness-control-using-ddcutil
           custom-hot-corners-extended
           smile-complementary-extension
+          advanced-alttab-window-switcher
         ];
     };
   };
